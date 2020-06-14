26c3f267de1a56e3357fd40cd94084a9b6719b92
Qinyun Wang <qinyun.wang@rea-group.com>
2020-06-14 21:58:58 +0800

    Complete practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,3 +1,23 @@
-const getPoetry = 0;
+/* eslint-disable no-console */
+const getPoem = (data) => {
+  // eslint-disable-next-line prefer-const
+  let result = [];
+  result.push(data.origin);
+  result.push(data.author);
+  result.push(data.content);
+  return result;
+};
+
+const getPoetry = async () => {
+  return fetch("https://v1.jinrishici.com/all.json", {
+    headers: {
+      "content-type": "application/json",
+    },
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((data) => getPoem(data))
+    .catch((error) => console.log(`Error: ${error}`));
+};
 
 export default getPoetry;
